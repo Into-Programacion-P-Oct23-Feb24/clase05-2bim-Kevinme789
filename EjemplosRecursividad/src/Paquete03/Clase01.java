b42d1de8475057ff3b8bc6d4a9a3191fc61c70fa
Kevin Robles <porkevin743@gmail.com>
2024-01-11 15:57:53 -0500

    Cambios

---
+++ EjemplosRecursividad/src/Paquete03/Clase01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete03;
+
+/**
+ *
+ * @author porke
+ */
+public class Clase01 {
+    public static void main(String[] args) {
+    
+    }
+    public static String[][] cadena() {
+        String[][] cadena = {{"LOJA", "CUENCA", "QUITO"},
+        {"GUAYAQUIL", "ZAMORA", "MACHALA"}};
+        return cadena;
+    }
+    
+}
